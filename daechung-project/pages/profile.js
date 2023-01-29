ebe9a3109d7d938149d9724d63ce90ca41051228
juhui88 <sand8594@hufs.ac.kr>
2023-01-29 16:58:13 +0900

    fix profile css

--- daechung-project/pages/profile.js
+++ daechung-project/pages/profile.js
@@ -20,25 +20,36 @@ const ErrorSpan = tw.span`
     text-xs 
     p-2
 `
-const Img = tw.img`
+const ProfileImg = tw.button`
     h-40
     w-40
     rounded-full
     cursor-pointer
+    bg-cover
+    focus:ring-2
+    ring-gray-500
+    transition
 `
 
 
 export default function Profile() {
     const {register, handleSubmit, formState:{errors}} = useForm();
     const {majorNum, setMajorNum} = useState(2);
+    const [isThirdMajor, setIsThirdMajor] = useState(false)
+    const [profileImgNum, setProfileImgNum] = useState(0);
+
     console.log(majorNum)
     const onValid = (data) => {
         console.log(data); // 나중에 여기서 백엔드로 옮기기
     }
 
-    const onClick = (e) => {
-        console.log(e)
-        addEventListener()
+    const onClickThird = () => {
+        if(!isThirdMajor){
+            setIsThirdMajor(true)
+        }
+        if(profileImgNum===0){
+
+        }
     }
     return <div className="lg:h-screen lg:flex">
         <div className=" m-10 lg:flex-grow">
@@ -46,8 +57,8 @@ export default function Profile() {
                 <span className="text-3xl font-bold text-gray-500">회원 정보 입력</span>
             </div>
             
-            <form className="grid gap-10 xl text-gray-500" onSubmit = {handleSubmit(onValid)}>
-                <button type="submit" className="absolute top-3 right-3 font-semibold ">시작하기➜</button>
+            <form className=" grid gap-10 xl text-gray-500" onSubmit = {handleSubmit(onValid)}>
+                <button type="submit" className="absolute top-3 right-3 font-semibold hover:scale-110 transition">시작하기➜</button>
                 <InputWrap>
                     <InputField>
                         <span>이름*:</span>
@@ -90,20 +101,29 @@ export default function Profile() {
                     <ErrorSpan>{errors?.ndMajor?.message}</ErrorSpan>
                     
                 </InputWrap>
+                {isThirdMajor?
+                <InputWrap>
+                    <InputField>
+                        <span>제3전공:</span>
+                        <Input type="text" {...register(`ndMajor`)}/>
+                        
+                    </InputField>
+                    <ErrorSpan>{errors?.rdMajor?.message}</ErrorSpan>
+                </InputWrap> :""}
             </form>
-            <div onClick={onClick} className="mx-20 text-right cursor-pointer">+</div>
+            <div onClick={onClickThird} className="mx-20 text-right cursor-pointer">+</div>
         </div>
         <div className="bg-bgPoint lg:flex-grow">
             <div className="p-10 text-3xl font-bold text-gray-500">
                 <span>프로필을 선택하세요 :&#41;</span>
             </div>
-            <div className="p-10 gap-8 grid grid-cols-3 justify-items-center">
-                <Img src="profile_1.png"/>
-                <Img src="profile_2.png"/>
-                <Img src="profile_3.png"/>
-                <Img src="profile_4.png"/>
-                <Img src="profile_5.png"/>
-                <Img src="profile_6.png"/>
+            <div className="p-10 gap-8 grid grid-cols-3 justify-items-center ">
+                <ProfileImg className="bg-[url('../public/profileImgs/profile_1.png')]" />
+                <ProfileImg className="bg-[url('../public/profileImgs/profile_2.png')]" />
+                <ProfileImg className="bg-[url('../public/profileImgs/profile_3.png')]" />
+                <ProfileImg className="bg-[url('../public/profileImgs/profile_4.png')]" />
+                <ProfileImg className="bg-[url('../public/profileImgs/profile_5.png')]" />
+                <ProfileImg className="bg-[url('../public/profileImgs/profile_6.png')]" />
             </div>
         </div>
     </div>
