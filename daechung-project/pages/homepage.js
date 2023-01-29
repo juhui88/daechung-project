c8daae004cb00b3359d88e87785f46d6367a8d59
juhui88 <sand8594@hufs.ac.kr>
2023-01-29 17:23:20 +0900

    update hoepage css

--- daechung-project/pages/homepage.js
+++ daechung-project/pages/homepage.js
@@ -1,3 +1,9 @@
 export default function Homepage(){
-    return null
+    return <div className="flex justify-between h-24">
+        <span className="flex items-end ml-5 text-3xl text-gray-700 font-normal">HOME</span>
+        <div className="p-5 flex flex-col-2 space-x-2 items-center h-10">
+            <div className="items-center w-6 h-6 rounded-full ring-1 ring-black"/>
+            <span className="text-xl" >⌵</span>
+        </div>
+    </div>
 }
