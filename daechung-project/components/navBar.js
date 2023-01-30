c02e6f9c6adba25a48d34e2ad4147dfc89b45b55
juhui88 <sand8594@hufs.ac.kr>
2023-01-31 02:01:18 +0900

    fix navBar css (cate)

--- daechung-project/components/navBar.js
+++ daechung-project/components/navBar.js
@@ -4,7 +4,7 @@ import tw from "tailwind-styled-components"
 const Lcate = tw.div`
     bg-itemBg
     p-2
-    pl-3
+    pl-1
     w-64
     flex
     justify-between
@@ -14,11 +14,16 @@ const Lcate = tw.div`
 const LCateName = tw.span`
     font-semibold
     text-textPoint
+    pl-1
 `
-const LCatePlusBtn = tw.button`
+const PlusBtn = tw.button`
+    opacity-0
+    group-hover:opacity-100
     text-gray-500
-    pl-2
-    hover:scale-125
+    hover:font-bold
+    hover:text-gray-700
+    hover:duration-300
+    w-3
 `
 const LcateFoldBtn = tw.button`
     opacity-0
@@ -28,43 +33,53 @@ const LcateFoldBtn = tw.button`
     text-gray-600
 `
 const Mcate = tw.div`
-    px-3 w-64 
     text-gray-600 
     grid 
     gap-2 
     font-medium
+    pl-1
+    group
+    w-64
 `
 const Scate = tw.div`
-    pl-5
+    pl-8
+    w-64
 `
 export default function NavBar() {
-    const [lCateBool, setLcateBool] = useState([
+    const [lCateBool, setLCateBool] = useState([
         {
             index:0,
-            bool:false
+            bool:true
         },
         {
             index:1,
-            bool:false
+            bool:true
         },
         {
             index:2,
-            bool:false
+            bool:true
         }
+    ])
+    const [mCateBool, setMCateBool] = useState([
+
     ])
     
     const onClickFold = (i) => {
-        setLcateBool(
+        setLCateBool(
             lCateBool.map(item=>
                 item.index===i ? {...item, bool: !item.bool}: item)
         )
     }
 
-    return <div className="grid gap-2 sc">
+    const onClickPlus = (i) => {
+
+    }
+    return <div className="grid gap-2 ">
         <Lcate>
-            <div>
+            <div className="">
+                <PlusBtn >+</PlusBtn>
                 <LCateName>교과</LCateName>
-                <LCatePlusBtn >+</LCatePlusBtn>
+                
             </div>
             <div onClick={()=>onClickFold(0)} >
                 <LcateFoldBtn>{lCateBool[0].bool ?"⏶" : "⏷"}</LcateFoldBtn>
@@ -73,7 +88,10 @@ export default function NavBar() {
         {lCateBool[0].bool ? null: 
         <div className="grid gap-2">
             <Mcate className="">
-                <div className=" "><span>1학년 1학기</span></div>
+                <div className=" ">
+                    <PlusBtn>+</PlusBtn>
+                    <span className="pl-1">1학년 1학기</span>
+                </div>
                 <Scate >
                     <span>
                         기업인수합병
@@ -87,7 +105,10 @@ export default function NavBar() {
             </Mcate>
             
             <Mcate className="">
-                <div className=" "><span>1학년 2학기</span></div>
+                <div className=" ">
+                    <PlusBtn>+</PlusBtn>        
+                    <span className="pl-1">1학년 2학기</span>
+                </div>
                 <Scate className=" ">
                     <span>
                         기업인수합병
@@ -99,23 +120,59 @@ export default function NavBar() {
         }
         <Lcate>
             <div>
+                <PlusBtn>+</PlusBtn> 
                 <LCateName>비교과</LCateName>
-                <LCatePlusBtn>+</LCatePlusBtn>    
+                   
             </div>
             <div onClick={()=>onClickFold(1)} >
                 <LcateFoldBtn>{lCateBool[1].bool ? "⏶": "⏷"}</LcateFoldBtn>
             </div>
             
         </Lcate>
+        {lCateBool[1].bool ? null: 
+        <div className="grid gap-2">
+            <Mcate className="">
+                <div className=" ">
+                    <PlusBtn>+</PlusBtn>
+                    <span className="pl-1">인턴</span>
+                </div>
+                <Scate >
+                    <span>
+                        (주) 무신사 고객관리 인턴 
+                    </span>
+                </Scate>
+            </Mcate>
+            
+            <Mcate className="">
+                <div className=" ">
+                    <PlusBtn>+</PlusBtn>        
+                    <span className="pl-1">동아리</span>
+                </div>
+            </Mcate>    
+        </div>
+        
+        }
         <Lcate>
             <div>
+                <PlusBtn>+</PlusBtn>
                 <LCateName>기타</LCateName>
-                <LCatePlusBtn>+</LCatePlusBtn>
+                
             </div>
             <div onClick={()=>onClickFold(2)} >
                 <LcateFoldBtn>{lCateBool[2].bool ? "⏶": "⏷"}</LcateFoldBtn>
             </div>
             
         </Lcate>
+        {lCateBool[2].bool ? null: 
+        <div className="grid gap-2">
+            <Mcate className="">
+                <div className=" ">
+                    <PlusBtn>+</PlusBtn>
+                    <span className="pl-1">기타</span>
+                </div>
+            </Mcate>   
+        </div>
+        
+        }
     </div>
 }
