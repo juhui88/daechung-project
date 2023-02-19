ecedea4554d12552374d8028c30f4397daa5a293
juhui88 <sand8594@hufs.ac.kr>
2023-02-19 13:14:19 +0900

    fix layout

--- daechung-project/components/laytout.js
+++ daechung-project/components/laytout.js
@@ -8,8 +8,8 @@ export default function Layout({children}) {
     
     
     return (
-    <div className="grid w-srceen h-screen wrap">
-        <div className="col-start-1 col-end-3 lg:col-end-4 row-start-1 row-end-2">
+    <div className="w-srceen h-screen ">
+        <div className="">
             <div className=" flex justify-between h-24">
                 <Link href = "/home" className="flex items-end ml-5">
                     <span className=" text-3xl text-gray-700 font-normal">HOME</span>
@@ -24,14 +24,16 @@ export default function Layout({children}) {
         </div>
         
         
-        <div className="col-start-1 col-end-2 row-start-2 row-end-3 ">
-            <NavBar/>
-        </div>
-        <div className="hidden lg:block lg:col-start-3 lg:col-end-4 lg:row-start-2 lg:row-end-3">
-            <CalendarBar />
-        </div>
-        <div className="col-start-2 col-end-3 row-start-2 row-end-3">
-            {children}    
+        <div className="flex ">
+                <NavBar/>
+            <div className="flex-1">
+                {children}    
+                </div>
+            <div className="hidden lg:block">
+                <CalendarBar />
+            </div>
+                
+                
         </div>
         <style jsx global>{`
             .wrap {
