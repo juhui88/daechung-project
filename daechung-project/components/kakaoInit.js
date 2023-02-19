b48404f261eb24101d3b4788feae50e82cfc0f13
juhui88 <sand8594@hufs.ac.kr>
2023-02-19 16:06:49 +0900

    fix 환경변수 설정

--- daechung-project/components/kakaoInit.js
+++ daechung-project/components/kakaoInit.js
@@ -1,7 +1,7 @@
 export const kakaoInit = () => {
     const kakao = (window).Kakao;
     if(!kakao.isInitialized()) {
-        kakao.init("1569518d3084739420e074d8befe8ec2");
+        kakao.init(process.env.KAKAO_KEY);
     }
 
     return kakao;
